5bab46c43e289d76f40a583bad24b3cdbf16e54c
Kevin Hernandez-Rives <rurunosep@gmail.com>
2020-06-10 21:46:07 -0400

    Toggle cards active/inactive to skip them in quiz

--- src/index.js
+++ src/index.js
@@ -1,81 +1,65 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-function EditableCardListRow({ card, saveCard, deleteCard }) {
-  const [fields, setFields] = useState(card)
+function EditableCardListRow({ card, editCard, deleteCard, setUneditable }) {
+  const [fields, setFields] = useState({ front: card.front, back: card.back })
 
   return (
-    <tr>
+    <>
       <td><input
         type='text'
-        name='front'
         value={fields.front}
         onChange={(e) => setFields({ ...fields, front: e.target.value })} /></td>
       <td><input
         type='text'
-        name='back'
         value={fields.back}
         onChange={(e) => setFields({ ...fields, back: e.target.value })} /></td>
-      <td><button onClick={() => saveCard(fields)}>Save</button></td>
+      <td><button onClick={() => {
+        editCard(fields)
+        setUneditable()
+      }}>Save</button></td>
       <td><button onClick={deleteCard}>Delete</button></td>
-    </tr>
+    </>
   )
 }
 
 function UneditableCardListRow({ card, setEditable }) {
   return (
-    <tr>
+    <>
       <td>{card.front}</td>
       <td>{card.back}</td>
       <td><button onClick={setEditable}>Edit</button></td>
-    </tr>
+    </>
   )
 }
 
 function CardsListRow({ card, editCard, deleteCard }) {
   const [editable, setEditable] = useState(false)
 
-  if (editable) {
-    return (
-      <EditableCardListRow
-        card={card}
-        saveCard={(newCard) => {
-          editCard(newCard)
-          setEditable(false)
-        }}
-        deleteCard={() => {
-          deleteCard()
-          setEditable(false)
-        }} />
-    )
-  } else {
-    return (
-      <UneditableCardListRow
-        card={card}
-        setEditable={() => setEditable(true)} />
-    )
-  }
-}
+  // TODO: rename/cleanup/whatever
+  const row = editable
+    ? <EditableCardListRow
+      card={card}
+      editCard={(changes) => {
+        editCard(changes)
+      }}
+      deleteCard={() => {
+        deleteCard()
+        setEditable(false)
+      }}
+      setUneditable={() => setEditable(false)} />
+    : <UneditableCardListRow
+      card={card}
+      setEditable={() => setEditable(true)} />
 
-function CardsList({ cards, editCard, deleteCard }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Front</th>
-          <th>Back</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cards.map((card, index) => (
-          <CardsListRow
-            card={card}
-            editCard={(newCard) => editCard(index, newCard)}
-            deleteCard={() => deleteCard(index)}
-            key={index} />
-        ))}
-      </tbody>
-    </table>
+    <tr>
+      <td><input
+        type='checkbox'
+        checked={card.active}
+        onChange={(e) => editCard({ active: e.target.checked })} /></td>
+      {row}
+    </tr>
   )
 }
 
@@ -90,13 +74,11 @@ function AddCardForm({ addCard }) {
     }}>
       <input
         type='text'
-        name='front'
         placeholder='Front'
         value={fields.front}
         onChange={(e) => setFields({ ...fields, front: e.target.value })} />
       <input
         type='text'
-        name='back'
         placeholder='Back'
         value={fields.back}
         onChange={(e) => setFields({ ...fields, back: e.target.value })} />
@@ -105,18 +87,54 @@ function AddCardForm({ addCard }) {
   )
 }
 
+function CardsList({ cards, addCard, editCard, deleteCard, setAllCardsActive }) {
+  return (
+    <div>
+      <AddCardForm addCard={addCard} />
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Front</th>
+            <th>Back</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cards.map((card, index) => (
+            <CardsListRow
+              card={card}
+              editCard={(changes) => editCard(index, changes)}
+              deleteCard={() => deleteCard(index)}
+              key={card.front} />
+          ))}
+        </tbody>
+      </table>
+      <button onClick={() => setAllCardsActive(true)}>All Active</button>
+      <button onClick={() => setAllCardsActive(false)}>All Inactive</button>
+    </div>
+  )
+}
+
 // TODO: rename
-function CardDisplay({ card, showNextCard }) {
+function CardDisplay({ card, showNextCard, editCard }) {
   const [flipped, setFlipped] = useState(false)
+
+  if (!card) card = { front: '', back: '' }
 
   return (
     <div>
       <h1>{flipped ? card.back : card.front}</h1>
-      <button onClick={() => setFlipped(!flipped)}>Flip Card</button>
+      <button onClick={() => setFlipped(!flipped)}>Flip</button>
       <button onClick={() => {
         showNextCard()
         setFlipped(false)
-      }}>Next Card</button>
+      }}>Next</button>
+      <button onClick={() => {
+        editCard({ active: false })
+        // TODO: updating state is async and hasn't finished yet. card is still active
+        // and if it was the only active one left, showNextCard will show the same one
+        showNextCard()
+      }}>Set Inactive</button>
       <hr></hr>
     </div>
   )
@@ -136,31 +154,46 @@ function App({ initialCards }) {
     <div>
       <CardDisplay
         card={cards[currentCardIndex]}
-        showNextCard={() => (
-          setCurrentCardIndex(currentCardIndex < cards.length - 1 ? currentCardIndex + 1 : 0)
-        )} />
+        showNextCard={() => {
+          //TODO: pull this out?
+          let nextCardIndex = null
+          if (cards.filter(card => card.active).length > 0) {
+            nextCardIndex = currentCardIndex
+            do {
+              nextCardIndex = nextCardIndex < cards.length - 1 ? nextCardIndex + 1 : 0
+            } while (!cards[nextCardIndex].active)
+          }
+          setCurrentCardIndex(nextCardIndex)
+        }}
+        editCard={(changes) => {
+          setCards(cards.map((card, i) => {
+            return currentCardIndex === i ? { ...card, ...changes } : card
+          }))
+        }} />
       <button onClick={() => setListVisible(!listVisible)}>
         {listVisible ? 'Hide List' : 'Show List'}
       </button>
       {listVisible &&
-        <div>
-          <AddCardForm
-            addCard={(card) => {
-              setCards([...cards, card])
-            }} />
-          <CardsList
-            cards={cards}
-            deleteCard={(index) => {
-              setCards(cards.filter((card, i) => {
-                return index !== i
-              }))
-            }}
-            editCard={(index, newCard) => {
-              setCards(cards.map((card, i) => {
-                return index === i ? newCard : card
-              }))
-            }} />
-        </div>}
+        <CardsList
+          cards={cards}
+          addCard={(card) => {
+            setCards([...cards, { active: true, ...card }])
+          }}
+          editCard={(index, changes) => {
+            setCards(cards.map((card, i) => {
+              return index === i ? { ...card, ...changes } : card
+            }))
+          }}
+          deleteCard={(index) => {
+            setCards(cards.filter((card, i) => {
+              return index !== i
+            }))
+          }}
+          setAllCardsActive={(active) => {
+            setCards(cards.map((card) => {
+              return { ...card, active: active }
+            }))
+          }} />}
     </div>
   )
 }
@@ -168,9 +201,9 @@ function App({ initialCards }) {
 let initialCards = JSON.parse(localStorage.getItem('cards'))
 if (!initialCards) {
   initialCards = [
-    { front: 'Dog', back: '개' },
-    { front: 'Cat', back: '고양이' },
-    { front: 'Fish', back: '물고기' },
+    { active: true, front: 'Dog', back: '개' },
+    { active: true, front: 'Cat', back: '고양이' },
+    { active: true, front: 'Fish', back: '물고기' },
   ]
 }
 
