57557d20cfaa2a9a3838541ef3e0aaec2ee8f93b
Kevin Hernandez-Rives <rurunosep@gmail.com>
2020-07-03 21:53:32 -0400

    Extracted TTS code into static class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import { TextToSpeechClient } from '@google-cloud/text-to-speech'
-import { google } from '@google-cloud/text-to-speech/build/protos/protos'
-import { JWT } from 'google-auth-library'
+import { TTS } from './tts.js'
 import './index.css'
 
 function EditableCardListRow({ card, editCard, deleteCard, setUneditable }) {
@@ -158,18 +156,18 @@ function CardsList({ cards, addCard, editCard, deleteCard, setAllCardsEnabled, s
   )
 }
 
-function TTSOptions({ languages, voices, selectedLanguage, selectedVoice, setLanguage, setVoice }) {
+function TTSOptions({ filteredVoices, selectedLanguage, selectedVoice, setLanguage, setVoice }) {
   return (
     <div style={{ marginTop: 5 }}>
       <label>Language: </label>
       <select value={selectedLanguage} onChange={(e) => setLanguage(e.target.value)}>
-        {languages.map((lang) =>
+        {TTS.languageCodes.map((lang) =>
           <option key={lang} value={lang}>{lang}</option>
         )}
       </select>
       <label>Voice: </label>
       <select value={selectedVoice} onChange={(e) => setVoice(e.target.value)}>
-        {voices.map((voice) =>
+        {filteredVoices.map((voice) =>
           <option key={voice.name} value={voice.name}>{voice.name}</option>
         )}
       </select>
@@ -178,12 +176,13 @@ function TTSOptions({ languages, voices, selectedLanguage, selectedVoice, setLan
 }
 
 // TODO: rename or restructure?
-function CardDisplay({ card, showNewCard, editCard, ttsClient, voices, languageCodes }) {
+function CardDisplay({ card, showNewCard, editCard }) {
   const [flipped, setFlipped] = useState(false)
-  const [selectedLanguage, setSelectedLanguage] = useState(languageCodes[0])
-  const [selectedVoice, setSelectedVoice] = useState(undefined)
+  // const [selectedLanguage, setSelectedLanguage] = useState(languageCodes[0])
+  const [selectedLanguage, setSelectedLanguage] = useState('en-AU')
+  const [selectedVoice, setSelectedVoice] = useState()
 
-  voices = voices.filter((voice) => voice.languageCodes.includes(selectedLanguage))
+  const filteredVoices = TTS.voices.filter((voice) => voice.languageCodes.includes(selectedLanguage))
     .sort((a, b) => {
       if (a.name < b.name) return -1
       if (a.name > b.name) return 1
@@ -191,27 +190,10 @@ function CardDisplay({ card, showNewCard, editCard, ttsClient, voices, languageC
     })
 
   useEffect(() => {
-    setSelectedVoice(voices[0])
+    setSelectedVoice(filteredVoices[0])
   }, [selectedLanguage])
 
   if (!card) card = { front: '', back: '' }
-
-  // TODO: maybe cache some results?
-  const playTTS = async function (text) {
-    if (!ttsClient || !selectedLanguage || !selectedVoice) return
-
-    const request = {
-      input: { text: text },
-      voice: { languageCode: selectedLanguage, name: selectedVoice },
-      audioConfig: { audioEncoding: google.cloud.texttospeech.v1.AudioEncoding.MP3 },
-    };
-
-    const [response] = await ttsClient.synthesizeSpeech(request)
-    const blob = new Blob([response.audioContent], { type: 'audio/mp3' })
-    const url = window.URL.createObjectURL(blob)
-    const audio = new Audio(url)
-    audio.play()
-  }
 
   return (
     <div>
@@ -247,17 +229,16 @@ function CardDisplay({ card, showNewCard, editCard, ttsClient, voices, languageC
           Disable
         </button>
         <button
-          onClick={() => { playTTS(flipped ? card.back : card.front) }}
-          disabled={!ttsClient}
+          onClick={() => TTS.play(flipped ? card.back : card.front, selectedLanguage, selectedVoice)}
+          disabled={!TTS.client}
         >
           Play
         </button>
       </div>
 
-      {ttsClient && (
+      {TTS.client && (
         <TTSOptions
-          languages={languageCodes}
-          voices={voices}
+          filteredVoices={filteredVoices}
           selectedLanguage={selectedLanguage}
           selectedVoice={selectedVoice}
           setLanguage={setSelectedLanguage}
@@ -268,7 +249,7 @@ function CardDisplay({ card, showNewCard, editCard, ttsClient, voices, languageC
   )
 }
 
-function App({ initialCards, ttsClient, voices, languageCodes }) {
+function App({ initialCards }) {
   const [cards, setCards] = useState(initialCards)
   const [listVisible, setListVisible] = useState(true)
   const [currentCardIndex, setCurrentCardIndex] = useState(0)
@@ -281,7 +262,6 @@ function App({ initialCards, ttsClient, voices, languageCodes }) {
     [cards]
   )
 
-  // TODO: do this more nicely
   useEffect(
     function showRandomCard() {
       if (shouldShowNewCard) {
@@ -315,9 +295,6 @@ function App({ initialCards, ttsClient, voices, languageCodes }) {
             )
           )
         }
-        ttsClient={ttsClient}
-        voices={voices}
-        languageCodes={languageCodes}
       />
 
       <div
@@ -364,40 +341,6 @@ function App({ initialCards, ttsClient, voices, languageCodes }) {
   )
 }
 
-// TODO: properly catch any errors in initialization
-async function initializeTTS() {
-  /* ~~~~~
-    ♡ Pretty please, no stealerino ♡
-  ~~~~~ */
-  const authJSON = {
-    type: "service_account",
-    project_id: "ruruflashcards",
-    private_key_id: "acceae75f2711f9eeb724520248a9cf779f8d8cc",
-    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDR/q75TPMsWUen\nBZg+FJQRCjSvPM1cpKl954Svp1hOIw2xjIHqXw1d/f1obXmgjgU4iYW8x/IJCfsK\nGUGWUF418ad1X113rGoaHQQpakjOubbeg9j4GAKjAeVz8XDK34oFYFJ997Bsl3rg\nm6np6eQ1+q2zKq2JH82aRMDD5YeBFUlTKPqfkRw4BPPdYHtYQhlh/vu+QbRww2tW\n0hsc4EY+GeC0AwUXRcjzY+UAreWRgM9uAJI+Lqv47iI//NmvEYKWdecIbYcruxKp\n4D8qTCdMrYPLF9kDK1jYUsklsqe51G4FZFwEXbOTV1GQBn133JVEzsLl1dHr2zCJ\nz7BXr1qvAgMBAAECggEARnjTbIuD4gK2Npl8jXzncc58fsCHZItH7B5Jm48r5dEC\ns+5k3Ov4Nu5ZX/W5RwXSP7Z7IK7zDVCBpFJ0fcbLzwuheJS/77z3QHszXdiyxVly\nwrr5kcyw+dZVk/LXOOYK0iIQnQCF/vNZA86Jl5vr/6d4KnPsl+OJ4rcm/7bkIg+Q\nexuGpLaONQiF2NIvQq/pi5HdZHq+iTF80tn7UfnuAm+J4jY6/npVRYXJXbunonzG\npPewvrGNGjCCjvGf0yPXKiOwKSkjuCTN4TH+VJZZSGV/rhmoYTkf7hZXDHihfEix\nQQ/UdomJTMljPPkpHsg/U/fRsIB2qpsSR0jtxSx5xQKBgQDwRS3M1VkdBzdxaSKN\nfLq09kAZc70j1h2ctrJPHa2YCFHJjOEmhEaN/twyHlXcVb1dMVnfwUwxeO5UZhwS\n3CI3VI18u3vMr0KVHCgtCIeWVeNnlaJ8yw8V2vqkfuTjgGYWpCY2Qo411SOam3Vl\nzJoDFKx7+zgi/XxXjK0mxEWQnQKBgQDfvhpaaIeFWZd/GQmxzy1SHhSBvCNfUOpC\nt4i7xvyIRY8NO5r6q5aM9xEenDyzl4nc01kIswqOhgKH34VuICa5mOHXUJz1CXUe\n1a7/TzUhZ8GeOWl7Z1JGPw29bpfUAkeRlmmTRwunRSDgF46kJQUNrBQgg1WOKg5/\n2NhtF5AYuwKBgQCpeuK3nbZiN3jwUozA6L56b0j/qxg7cwkoRea4z+JnX1bxqKIY\nnS13c9K2t5cw+Hm+htUydBLewsK6XdxnoUexZ771wPmug+GfdGESgvXBIYxqwK4B\nAOr/K5uo9KlXoHZieh9KHuBZMKMQp5/D0vLAQZD5U1dhtxRCXUS2F7RKMQKBgQCf\nOLusRuLaRM2IxxqdDKBl5b4WLPrHI9/xpoaJiqu/ljCc7CP36w/yNQhbzjdsXpTf\nLxAXHsKOdlNquehMXFjyjxd4kIeB4T8VuF8WlRlsMlgY7yZfiUGFd+2hNwiY+R5R\nPsbW5iIm4QzqLBl4OlgESMbx9ER4LPmwhXJPAAutbQKBgQDPJLj6dIVLoimBydpv\nlv8V94Hfh3oLSoaMIu5ONOUjbYReE5/gZfsJ9wv2BKEmdhxTcDyEEo4Vjx1MK0zD\nk2uWGNebIkjHY+um/rRVzc/TRAUR4GSwWjzX3NNzVTxkhMj11zxpyruNU3Vo+UwU\ns7LkwlgtHEbLEa59eP+tojg8OA==\n-----END PRIVATE KEY-----\n",
-    client_email: "ruruflashcards-user@ruruflashcards.iam.gserviceaccount.com",
-    client_id: "108355697865110828466",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/ruruflashcards-user%40ruruflashcards.iam.gserviceaccount.com"
-  }
-
-  const authClient = new JWT({
-    email: authJSON.client_email,
-    key: authJSON.private_key,
-    scopes: ['https://www.googleapis.com/auth/cloud-platform']
-  })
-
-  const ttsClient = new TextToSpeechClient({ auth: authClient })
-
-  const [response] = await ttsClient.listVoices()
-  const voices = response.voices
-  const languageCodes =
-    [...new Set(voices.map((v) => v.languageCodes).reduce((flat, x) => [...flat, ...x]))].sort()
-
-  return { ttsClient, voices, languageCodes }
-}
-
 async function main() {
   let initialCards = JSON.parse(localStorage.getItem('cards'))
   if (!initialCards) {
@@ -408,18 +351,10 @@ async function main() {
     ]
   }
 
-  // If initialization failed, these should all be undefined. Probably.
-  let { ttsClient, voices, languageCodes } = initializeTTS()
-  voices = voices ? voices : []
-  languageCodes = languageCodes ? languageCodes : []
+  await TTS.initialize()
 
   ReactDOM.render(
-    <App
-      initialCards={initialCards}
-      ttsClient={ttsClient}
-      voices={voices}
-      languageCodes={languageCodes}
-    />,
+    <App initialCards={initialCards} />,
     document.getElementById('root')
   )
 }
